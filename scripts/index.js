5ba74446866b49b0b556552ebd7b6ae5ddba1075
Alejandro Melendez <systemale@gmail.com>
2025-07-07 23:48:00 +0200

    feat: Agregar Tarjeta

--- scripts/index.js
+++ scripts/index.js
@@ -8,17 +8,75 @@ let jobInput = document.querySelector("#about-me");
 let profileName = document.querySelector(".profile__name");
 let profileAbout = document.querySelector(".profile__about-me");
 
+// Seleccionamos los elementos relacionados con el formulario clonado
 let btnAddForm = document.querySelector(".profile__button-add");
 let contentFormClone = document.querySelector("#form-clone");
 
-//Modificar formulario clonado Add
+//Seleccionar contenedor para agregar tarjetas
+let cards = document.querySelector(".elements");
 
+// Función para crear una tarjeta
+function addCard(cardTitle, cardImage) {
+  const cardTemplate = document.querySelector("#elements-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".elements__card")
+    .cloneNode(true);
+
+  cardElement.querySelector(".elements__card-title").textContent = cardTitle;
+  cardElement
+    .querySelector(".elements__card-image")
+    .setAttribute("src", cardImage);
+
+  cardElement
+    .querySelector(".elements__card-image")
+    .setAttribute("alt", `fotografia de: ${cardTitle}`);
+
+  cards.prepend(cardElement);
+}
+
+// tarjetas iniciales
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "./images/card__image_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+// Agregar tarjetas iniciales
+initialCards.forEach((card) => {
+  addCard(card.name, card.link);
+});
+
+// Evento para clonar y abrir formulario nuevo
 btnAddForm.addEventListener("click", () => {
   const formClone = popup.cloneNode(true);
 
+  // Cambiamos el texto del popup
   formClone.querySelector(".popup__title").textContent = "Nuevo Lugar";
   formClone.querySelector(".popup__save").textContent = "Crear";
 
+  // Limpiamos los inputs y cambiamos placeholders
   const inputs = formClone.querySelectorAll("input");
   inputs.forEach((input) => {
     inputs.value = "";
@@ -26,17 +84,29 @@ btnAddForm.addEventListener("click", () => {
     inputs[1].placeholder = "Enlace a la Imagen";
   });
 
+  // Botón cerrar del formulario clonado
   const closeBtnClon = formClone.querySelector(".popup__button-close");
   closeBtnClon.addEventListener("click", () => {
     formClone.remove();
   });
 
+  // Evento submit del formulario clonado
   const formInClone = formClone.querySelector("form");
   formInClone.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    //Obtenemos los valores ingresados al momento del submit
+    const cardTitle = inputs[0].value;
+    const cardImage = inputs[1].value;
+
+    // Creamos la tarjeta
+    addCard(cardTitle, cardImage);
+
+    // Cerramos y eliminamos el formulario
     formClone.remove();
   });
 
+  // Mostramos el formulario clonado
   formClone.classList.add("popup_opened");
   contentFormClone.append(formClone);
 });
